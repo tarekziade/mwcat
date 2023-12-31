5ddb24f8030d15418be20f1ed1730ab7402d2adf
Tarek Ziadé <tarek@ziade.org>
2023-12-31 14:55:35 +0100

    reworked the README

--- README.md
+++ README.md
@@ -1,107 +1,35 @@
 # mwcat
 
-Project to create a text classification model using Wikipedia's top categories and
-see if they can be used on any web page out there.
+Project to create two models using Wikipedia (English).
+
+- a text classifier, based on Wikipedia top 40 categories.
+- a text summarizer, based on Wikipedia summaries.
 
 Wikipedia has 40 top categories that cover all popular topics.
 It's a much better taxonomy than the classical AG News or Reuters one.
 
 See https://en.wikipedia.org/wiki/Category:Main_topic_classifications
 
-If the produced model is able to classify any web page against those topics,
-it can be used in Firefox to group tabs per user intent.
+The project comes with a few scripts:
 
-The theory is that a user intent can be inferred from the categories from each page.
-
-Proposed list of user intents / wikipedia topics matches :
-
-1. **Researching for Academic Purposes**:
-
-   - Academic disciplines, Science, Mathematics, Technology, Humanities, History, Philosophy
-
-2. **Gathering News and Current Events**:
-
-   - Mass media, Politics, Government, Society, Economy, International Relations, Culture
-
-3. **Personal Entertainment**:
-
-   - Entertainment, Sports, Culture, Music, Television, Film, Gaming
-
-4. **Business and Financial Information Seeking**:
-
-   - Business, Economy, Entrepreneurship, Corporate News, Stock Market, Industry Trends
-
-5. **Educational and Learning Activities**:
-
-   - Education, Academic disciplines, Science, Mathematics, Humanities, Language, Engineering
-
-6. **Health and Wellness Information**:
-
-   - Health, Medicine, Nutrition, Exercise, Mental Health, Diseases, Health Policies
-
-7. **Shopping or Product Research**:
-
-   - Business, Economy, Consumer Goods, Technology, Fashion, Food and Drink
-
-8. **Travel Planning**:
-
-   - Geography, Culture, Tourism, International Destinations, Local Attractions, Travel Tips
-
-9. **Legal Information Search**:
-
-   - Law, Government Policies, Legal Cases, Civil Rights, Legislation
-
-10. **Environmental and Nature Exploration**:
-
-    - Nature, Environment, Geography, Biology, Conservation, Climate Change
-
-11. **Social Interaction and Communication**:
-
-    - Communication, Social Media, Internet, Society, Culture, Human Behavior
-
-12. **Exploring Philosophical and Ethical Questions**:
-
-    - Philosophy, Ethics, Religion, Society, Humanities, History
-
-13. **Understanding Global and Political Affairs**:
-
-    - Politics, Government, International Relations, Law, Society, History
-
-14. **Technology and Gadget Information**:
-
-    - Technology, Engineering, Internet, Innovation, Gadgets, Computers
-
-15. **Cultural and Artistic Exploration**:
-
-    - Culture, Art, Music, Literature, Entertainment, History, Society
-
-16. **Sports and Physical Activity Information**:
-
-    - Sports, Health, Physical Fitness, International Competitions, Teams and Players
-
-17. **Historical Information and Research**:
-
-    - History, Culture, Geography, Politics, Military, Famous Historical Events
-
-18. **Science and Research Updates**:
-
-    - Science, Technology, Medical Research, Space Exploration, Physics, Biology
-
-19. **Exploring Outer Space and Astronomy**:
-
-    - Universe, Astronomy, Space Exploration, Physics, Science
-
-20. **Career and Job Search**:
-    - Business, Education, Career Development, Corporate Culture, Employment Trends
+- `mwcat-create-dataset`: extract pages from the Wikipedia and store them in a dataset
+- `mwcat-train`: use the dataset to train the classifier or summarizer.
+- `mwcat-evaluate`: evaluate the produced models using the model eval mode.
+- `mwcat-validate`: validate the produced models against the test data.
 
 ## Training Dataset
 
-The training (90% split) & test (10% split) dataset is composed of wikipedia pages titles and summaries.
+The training (90% split) & test (10% split) dataset is composed of wikipedia pages.
 
-Pages are selected directly under every root categories or their direct subcategories, ensuring a wide coverage of topics.
-The tree of categories is visited until each root category has a corpus of at most 1000 pages.
+Each page is composed of : id, title, summary and text.
 
-To generate the dataset, run `mwcat-create-dataset`. The script generates and uploads the dataset to the Hugging Face Hub at `tarekziade/wikipedia-topics`
+Pages are selected directly under every root categories or their direct subcategories, ensuring a
+wide coverage of topics and an even distribution of pages.
+The tree of categories is visited until each root category has a corpus of 2000 pages.
+
+To generate the dataset, run `mwcat-create-dataset`.
+
+The script generates and uploads the dataset to the Hugging Face Hub at https://huggingface.co/datasets/tarekziade/wikipedia-topics
 
 To use it:
 
@@ -111,11 +39,38 @@ from datasets import load_dataset
 dataset = load_dataset("tarekziade/wikipedia-topics")
 ```
 
-## Training
+## Classification
 
-Training is done by fine tuning DistilBERT. To train, run `mwcat-train`. The script trains and uploads the model to
-the Hugging Face Hub at `tarekziade/wikipedia-topics-distilbert`
-
-## Evaluation & Validation
+The classification model can be downloaded from https://huggingface.co/tarekziade/wikipedia-topics-distilbert
 
 XXX
+
+### Training
+
+Training is done by fine tuning DistilBERT.
+
+XXX provide results from the script
+
+### Evaluation & Validation
+
+XXX provide results from the script
+
+### Usage
+
+TBD
+
+XXX write an example using pytorch and transformers.js
+
+## Summarization
+
+### Training
+
+TDB
+
+### Evaluation & Validation
+
+TBD
+
+### Usage
+
+TBD
--- mwcat/create_dataset.py
+++ mwcat/create_dataset.py
@@ -7,10 +7,12 @@
     Author: Tarek Ziadé / Mozilla
 
 """
+import queue
+import threading
+import sqlite3
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from threading import Lock
-import shelve
 
 import wikipediaapi
 from datasets import Dataset, DatasetDict
@@ -20,7 +22,7 @@ from tqdm import tqdm
 from mwcat.utils import CATEGORIES, NUM_CATEGORIES, sentences
 
 
-_LIMIT_PER_CAT = 1000
+_LIMIT_PER_CAT = 2000
 
 
 class WikiExtractor:
@@ -30,7 +32,50 @@ class WikiExtractor:
         self.client = wikipediaapi.Wikipedia("MediaWikiCat Project", "en", timeout=30)
         self.data_lock = Lock()
         self.pbar = None
-        self.page_cache = shelve.open("pages.shelve", writeback=True)
+        self.db_connections = threading.local()
+        self.create_tables()
+        self.task_queue = queue.Queue()
+        self.workers = []
+
+    def get_db_connection(self):
+        if not hasattr(self.db_connections, "conn"):
+            self.db_connections.conn = sqlite3.connect("pages.db")
+        return self.db_connections.conn
+
+    def start_workers(self, num_workers):
+        for _ in range(num_workers):
+            thread = threading.Thread(target=self.worker)
+            thread.start()
+            self.workers.append(thread)
+
+    def stop_workers(self):
+        for _ in range(len(self.workers)):
+            self.task_queue.put(None)
+        for worker in self.workers:
+            worker.join()
+
+    def worker(self):
+        while True:
+            task = self.task_queue.get()
+            if task is None:  # Sentinel to indicate end of tasks
+                self.task_queue.task_done()
+                break
+            self.process_category(*task)
+            self.task_queue.task_done()
+
+    def create_tables(self):
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS pages (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            summary TEXT,
+            text TEXT,
+            categories TEXT
+        );
+        """
+        conn = self.get_db_connection()
+        conn.execute(create_table_query)
+        conn.commit()
 
     def process_category(
         self,
@@ -89,35 +134,83 @@ class WikiExtractor:
         category_name = category_name.removeprefix("Category:")
 
         with self.data_lock:
-            page_data = self.page_cache.get(
-                page_id,
-                {
-                    "title": page.title,
-                    "id": page.pageid,
-                    "summary": self.extract_summary(page),
-                    "categories": set(),
-                },
+            conn = self.get_db_connection()
+            page_query = (
+                "SELECT title, summary, text, categories FROM pages WHERE id = ?"
             )
-            page_data["categories"].add(category_name)
-            self.page_cache[page_id] = page_data
-            self.page_cache.sync()
-            return page_data
+            cursor = conn.execute(page_query, (page_id,))
+            row = cursor.fetchone()
+            if row:
+                title = row[0]
+                summary = row[1]
+                text = row[2]
+                existing_categories = set(row[3].split(","))
+                existing_categories.add(category_name)
+                categories = existing_categories
+
+                update_query = "UPDATE pages SET categories = ? WHERE id = ?"
+                conn.execute(update_query, (",".join(existing_categories), page_id))
+            else:
+                # Insert new page
+                title = page.title
+                text = page.text
+                summary = self.extract_summary(page)
+                insert_query = "INSERT INTO pages (id, title, summary, categories) VALUES (?, ?, ?, ?)"
+                conn.execute(
+                    insert_query, (page_id, page.title, summary, category_name)
+                )
+                categories = [category_name]
+
+            conn.commit()
+
+            return {
+                "id": page_id,
+                "summary": summary,
+                "title": title,
+                "categories": categories,
+                "text": text,
+            }
 
     def __call__(self):
         with tqdm(
-            total=NUM_CATEGORIES * _LIMIT_PER_CAT, desc="Processing Categories"
+            total=NUM_CATEGORIES * _LIMIT_PER_CAT, desc="Extracting data from wikipedia"
         ) as pbar:
             self.pbar = pbar
-            with ThreadPoolExecutor(max_workers=15) as executor:
-                for category in CATEGORIES:
-                    category = f"Category:{category}"
-                    executor.submit(self.process_category, category, category)
-                executor.shutdown(wait=True)
+            self.start_workers(15)
 
-            for page_data in self.page_cache.values():
+            for category in CATEGORIES:
+                category = f"Category:{category}"
+                self.task_queue.put((category, category))
+
+            self.task_queue.join()  # Wait for all tasks to be completed
+            self.stop_workers()
+            # for category in CATEGORIES:
+            #    category = f"Category:{category}"
+            #    self.process_category(category, category)
+
+        with tqdm(
+            total=NUM_CATEGORIES * _LIMIT_PER_CAT, desc="Creating dataset"
+        ) as pbar:
+            select_query = "SELECT id, title, summary, text, categories FROM pages"
+            cursor = self.get_db_connection().execute(select_query)
+
+            while True:
+                row = cursor.fetchone()
+                if row is None:
+                    break
+
+                page_data = {
+                    "id": row[0],
+                    "title": row[1],
+                    "summary": row[2],
+                    "text": row[3],
+                    "categories": set(row[4].split(",")),
+                }
                 yield page_data
+                pbar.update(1)
 
-        self.page_cache.close()
+        if hasattr(self.db_connections, "conn"):
+            self.db_connections.conn.close()
 
 
 def main():
